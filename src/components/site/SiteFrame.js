2a6075daa184bbea722c3a8f4589237220a05e53
Kent Brown <randall.k.brown.jr@gmail.com>
2020-06-15 14:47:06 -0400

    First development for site frontend. Small file structure changes

---
+++ src/components/site/SiteFrame.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import data from '../../data/DummySiteData.js';
+import './SiteFrame.css';
+
+class SiteFrame extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            text: null
+        }
+    }
+
+    componentDidMount() {
+        console.log(data);
+        this.setState(data);
+    }
+
+    render() {
+        return (
+            <div className="site-frame">
+                {this.state.text}
+            </div>
+        );
+    }
+}
+
+export default SiteFrame;
