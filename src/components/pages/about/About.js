932c1f7d69a4ba858b203b7c434ea17d6df2f9cc
Kent Brown <randall.k.brown.jr@gmail.com>
2020-11-20 18:26:27 -0500

    Added processing to display hobbies

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -10,11 +10,24 @@ class About extends React.Component {
         }
     }
 
+    renderKeys(obj) {
+        const values = Object.values(obj);
+        let renderedString = values[0].toString();
+        values.forEach(value => {
+            if (value === values[values.length - 1]) {
+                renderedString += ", and " + value.toString();
+            } else if (value !== values[0]) {
+                renderedString += ", " + value.toString();
+            }
+        });
+        return renderedString;
+    }
+
     render() {
         const data = this.state.data;
         return (
             <div className="about-container">
-                You found {data.sName}'s personal site! {data.name} is a fifth-year Software Engineering undergraduate at the Rochester Institute of Technology. {data.sName} currently lives in {data.basedIn} with his lovely roommates.
+                You found {data.sName}'s personal site! {data.name} is a fifth-year Software Engineering undergraduate at the Rochester Institute of Technology. {data.sName} currently lives in {data.basedIn} with his lovely roommates. His hobbies include {this.renderKeys(data.hobbies)}, though he's not particularly good at any of them.
             </div>
         );
     }
