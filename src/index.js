3840cdae875838e5bd2267371b85f5f64cef87b6
Kent Brown <randall.k.brown.jr@gmail.com>
2023-07-02 00:08:43 -0500

    Fleshed-out lightweight site

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 import LightweightSite from "./new-components/LightweightSite";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <StrictMode>
     <LightweightSite />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
--- src/new-components/LightweightSite.js
+++ src/new-components/LightweightSite.js
@@ -1,17 +1,23 @@
+import React from "react";
 import "./LightweightSite.css";
 
 export default function LightweightSite() {
-    return <div className="_background">
-        <div className="_site">
+    return <div className="_site">
             <Frontpage/>
-        </div>
-    </div>;
+        </div>;
   }
   
 function Frontpage() {
     return <>
-        <h1>Randall Kent Brown Jr.</h1>
-        Kent Brown is a Software Developer living in Madison, Wisconsin.
-        He currently works at Epic, an electronic health records company.
+        <div className="_headingContainer">
+            <h1 className="_heading">Randall Kent Brown Jr.</h1>
+            <h2 className="_heading">Madison, WI</h2>
+        </div>
+        <p>
+            Hello, I'm Kent! I'm a Software Developer currently working at Epic, an electronic health records company. I graduated with a Bachelor's in Software Engineering from RIT (Rochester Institute of Technology) in 2021.
+        </p>
+        <p>
+            "A quote? I don't know... I don't know a quote." -Carly Masonheimer, July 2023
+        </p>
     </>;
 }
